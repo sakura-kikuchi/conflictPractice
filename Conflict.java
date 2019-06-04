a86148035a7e9b2f57408fd2b21a1bb28f917e6c
sakura kikuchi <s.kikuchi1811@morijyobi.ac.jp>
2019-06-04 11:13:07 +0900

    [modify]Conflict.java

--- Conflict.java
+++ Conflict.java
@@ -1,7 +1,10 @@
 
-public class Hello {
+public class Conflict {
 	public static void main(String... args) {
-		System.out.println("Hello World");
+
+		for(int i = 30 ; i > 0 ; i-- ){
+		System.out.println(i);
+		}
 	}
 }
 
